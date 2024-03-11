70661021c1fea448c30da25db7e7b1eda89287b6
Rustem <mametich@rambler.ru>
2024-03-11 22:48:20 +0300

    KS-1-2 create variables employees

---
+++ src/main/kotlin/lesson_1/Lesson1_task2.kt
@@ -0,0 +1,14 @@
+package lesson_1
+
+
+fun main(){
+
+    var numberOfOrders = 0
+    val textGratitude = "Thank you for your order"
+    println(numberOfOrders)
+    println(textGratitude)
+
+    //var numberOfEmployees = 2000
+    var numberOfEmpoyees = 1999
+    println(numberOfEmpoyees)
+}

916a8cb25054b52cda9860b612fb0cc806fe82ea
Rustem <mametich@rambler.ru>
2024-03-11 23:03:26 +0300

    KS-1-3 create variables flight

---
+++ src/main/kotlin/lesson_1/Lesson1_task3.kt
@@ -0,0 +1,12 @@
+package lesson_1
+
+fun main(){
+    val yearOfFlight = "Год полета - 1961"
+    var hourAndMinute = "Взлет - 09:07"
+    println(yearOfFlight)
+    println(hourAndMinute)
+
+    hourAndMinute = "Посадка - 10:55"
+
+    println(hourAndMinute)
+}

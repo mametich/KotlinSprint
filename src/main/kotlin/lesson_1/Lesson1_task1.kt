fa45128b93460677460c11cb8f2f52ac401ff018
Rustem <mametich@rambler.ru>
2024-03-12 20:33:36 +0300

    KS-1-1 create new variables after refactoring

--- src/main/kotlin/lesson_1/Lesson1_task1.kt
+++ src/main/kotlin/lesson_1/Lesson1_task1.kt
@@ -1,6 +1,8 @@
 package lesson_1
 
-fun main(){
-    var numberOfOrders = 0
-    val textGratitude = "Thank you for your order"
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val textGratitude: String = "Thank you for your order"
+
 }

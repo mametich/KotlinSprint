f5fe8facc0f9550470ec49c1731e545b5786bfe0
Rustem <mametich@rambler.ru>
2024-03-13 21:29:50 +0300

    KS-1-5 create const variables

--- src/main/kotlin/lesson_1/Lesson1_task5.kt
+++ src/main/kotlin/lesson_1/Lesson1_task5.kt
@@ -1,12 +1,15 @@
 package lesson_1
 
+private const val SECOND_IN_SPASE = 6480
+private const val SECONDS_IN_MINUTE = 60
+private const val SECONDS_IN_HOUR = 3600
+
 fun main() {
 
-    val secondsInSpace = 6480
 
-    val minutesInSpace = secondsInSpace % 60
-    val hoursInSpace = (secondsInSpace % 3600)/60
+    val minutesInSpace = SECOND_IN_SPASE % SECONDS_IN_MINUTE
+    val hoursInSpace = (SECOND_IN_SPASE % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
 
-    println("0${secondsInSpace/3600}:$hoursInSpace:0${minutesInSpace}")
+    println("0${SECOND_IN_SPASE/3600}:$hoursInSpace:0${minutesInSpace}")
 
 }

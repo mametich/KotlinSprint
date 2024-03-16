a6db132a32babcdab262dfecec544261597e44d1
Rustem <mametich@rambler.ru>
2024-03-16 16:01:03 +0300

    KS-3-1 Hello

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main () {
+
+    val name = "Rustem"
+    val textHelloDay = "добрый день"
+    val textHelloEvening = "добрый вечер"
+
+    println("$name $textHelloDay, $name $textHelloEvening")
+
+}

2f6d9b70ab2a57a3cc8da56bf34e1e9546b7be0e
Rustem <mametich@rambler.ru>
2024-03-17 17:16:31 +0300

    KS-2-5 finally procent

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,19 +2,15 @@ package lesson_2
 
 import kotlin.math.pow
 
-private const val MONTH_IN_YEAR = 12
-
 fun main() {
 
     val sumOfClientRub = 70000
     val allYears = 20
     val procent = 16.7
 
-    val formulaProcent = (1 + (procent / 100.00) / 12)
-    val newNumber = formulaProcent.pow(MONTH_IN_YEAR * allYears)
-    val procentAfterYears = sumOfClientRub * newNumber
-    val formatedRpocent = "%.3f".format(procentAfterYears)
+    val sum = sumOfClientRub * (1 + procent / 100).pow(allYears)
+    val formattedSum = "%.3f".format(sum)
 
-    println("Размер начисленных процентов: $formatedRpocent")
+    println("Размер начисленных процентов: $formattedSum")
 
 }

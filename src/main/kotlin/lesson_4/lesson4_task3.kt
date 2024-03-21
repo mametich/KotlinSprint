c9c76e750329338a1f7c9120e05c2a1b6eb9e114
Rustem <mametich@rambler.ru>
2024-03-21 21:05:18 +0300

    KS-4-3 comparison

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,20 +1,20 @@
 package lesson_4
 
-fun main() {
+private const val IS_WEATHER_SUNNY = true
+private const val IS_AWNING_IS_OPEN = true
+private const val AIR_HUMIDITY_IN_PROCENT = 20
+private const val SEASON = "зима"
 
-    val isWeatherSunny: Boolean = true
-    val isAwningIsOpen: Boolean = true
-    val airHumidityInProcent: Int = 20
-    val season: String = "зима"
+fun main() {
 
     val isWeatherTodaySunny = true
     val isAwningIsOpenToday = true
     val airHumidityInProcentToday = 20
     val seasonToday = "не зима"
 
-    val isFavorableConditions =
-        isWeatherSunny == isWeatherTodaySunny && isAwningIsOpen == isAwningIsOpenToday
-                && airHumidityInProcent == airHumidityInProcentToday && season == seasonToday
+    val isFavorableConditions = IS_WEATHER_SUNNY == isWeatherTodaySunny &&
+            IS_AWNING_IS_OPEN == isAwningIsOpenToday &&
+            AIR_HUMIDITY_IN_PROCENT == airHumidityInProcentToday && SEASON == seasonToday
 
     println("Благоприятные ли условия сейчас для роста бобовых? $isFavorableConditions")
 

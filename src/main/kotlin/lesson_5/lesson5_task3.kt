f0bd26c64595a9b14a7a6b6f2f706b8408e078a8
Rustem <mametich@rambler.ru>
2024-03-24 10:29:03 +0300

    KS-5-3-comparison

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -5,15 +5,25 @@ fun main() {
     val firstCorrectNumber = 12
     val secondCorrectNumber = 35
 
-    println("Введите первое число от 0 до 42:")
+    println("Введите число от 0 до 42:")
     val firstNumberFromUser = readln().toInt()
 
-    println("Введите второе число от 0 до 42:")
+    println("Введите число от 0 до 42:")
     val secondNumberFromUser = readln().toInt()
 
-    if (firstNumberFromUser == firstCorrectNumber && secondNumberFromUser == secondCorrectNumber) {
+    if (
+        firstNumberFromUser == firstCorrectNumber ||
+        firstNumberFromUser == secondCorrectNumber &&
+        secondNumberFromUser == firstCorrectNumber ||
+        secondNumberFromUser == secondCorrectNumber)
+    {
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if (firstNumberFromUser == firstCorrectNumber || secondNumberFromUser == secondCorrectNumber) {
+    } else if (
+        firstNumberFromUser == firstCorrectNumber ||
+        firstNumberFromUser == secondCorrectNumber ||
+        secondNumberFromUser == secondCorrectNumber ||
+        secondNumberFromUser == firstCorrectNumber)
+    {
         println("Правильные цифры: $firstCorrectNumber и $secondCorrectNumber")
         println("Вы выиграли утешительный приз!")
     } else {

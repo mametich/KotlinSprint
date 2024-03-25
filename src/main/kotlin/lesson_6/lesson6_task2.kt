73c6dbf755b0aaf3152981c982caa9d792f3ca46
Rustem <mametich@rambler.ru>
2024-03-25 18:51:10 +0300

    KS-6-2-while

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -5,12 +5,10 @@ private const val MILLISECONDS_IN_SECOND = 1000
 fun main() {
 
     println("Введите количество секунд от 0 до 10")
-    var secondsFromUser = readln().toInt()
+    val secondsFromUser = readln().toInt()
     val secondsSleep = secondsFromUser * MILLISECONDS_IN_SECOND
 
-    while (secondsFromUser > 0) {
         Thread.sleep(secondsSleep.toLong())
         println("Прошло $secondsFromUser секунд")
-        secondsFromUser = 0
-    }
+
 }

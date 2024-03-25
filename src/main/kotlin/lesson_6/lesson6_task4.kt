a637193c33feb7e02a4d89cb5496c5b6fd598eb1
Rustem <mametich@rambler.ru>
2024-03-25 13:07:15 +0300

    KS-6-4-while

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -3,16 +3,23 @@ package lesson_6
 fun main() {
 
     val correctNumber = (1..9).random()
-    var quantityOfOpportunity = 0
+    var quantityOfOpportunity = 4
+    var counter = 0
 
     do {
         println("Введите число:")
         val numberFromUser = readln().toInt()
 
-        val isGuessedNumber =
-            numberFromUser == correctNumber
-        quantityOfOpportunity++
+        val isGuessedNumber = numberFromUser == correctNumber
 
-    } while (!isGuessedNumber && quantityOfOpportunity < 5)
-        println("Это была великолепная игра")
+        if (isGuessedNumber) counter = 0 else counter++
+
+        when (counter) {
+            0 -> println("Вы угадали. Это была великолепная игра")
+            in 1..4 -> println("Неверно. Количество попыток ${quantityOfOpportunity--}")
+            else -> println("Было загадано число $correctNumber")
+        }
+
+    } while (!isGuessedNumber && counter < 5)
+
 }

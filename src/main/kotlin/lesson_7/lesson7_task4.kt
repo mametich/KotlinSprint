255f2c95ba9e50b56f93bdb6e0ecef9ef9a5cf2a
Rustem <mametich@rambler.ru>
2024-03-26 14:38:19 +0300

    KS-7-4-for

---
+++ src/main/kotlin/lesson_7/lesson7_task4.kt
@@ -0,0 +1,17 @@
+package lesson_7
+
+private const val MILLISECONDS_IN_SECONDS = 1000
+
+fun main() {
+
+    println("Введите число")
+    val numberFromUser = readln().toInt()
+
+    for (i in numberFromUser downTo 1){
+        Thread.sleep(MILLISECONDS_IN_SECONDS.toLong())
+        println("Осталось $i секунд")
+    }
+    Thread.sleep(MILLISECONDS_IN_SECONDS.toLong())
+    println("Время вышло")
+
+}

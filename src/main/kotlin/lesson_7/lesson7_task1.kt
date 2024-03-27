1e76b17df811d4439bf0066151e8e04b498d157d
Rustem <mametich@rambler.ru>
2024-03-27 11:17:01 +0300

    KS-7-1-for

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -2,14 +2,7 @@ package lesson_7
 
 fun main() {
 
-    val chars1 = ('a'..'z').random()
-    val numbers1 = (0..9).random()
+    val chars1 = (('a'..'z') + (0..9)).shuffled().take(6).joinToString("")
+    println("Ваш пароль: $chars1")
 
-    val chars2 = ('a'..'z').random()
-    val numbers2 = (0..9).random()
-
-    val  chars3 = ('a'..'z').random()
-    val numbers3 = (0..9).random()
-
-    println("Ваш пароль: $chars1$numbers1$chars2$numbers2$chars3$numbers3")
 }

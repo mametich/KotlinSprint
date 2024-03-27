1245bcf41c122ca1993e6215f64f03d5a75c69b5
Rustem <mametich@rambler.ru>
2024-03-27 11:22:51 +0300

    KS-7-4-for

--- src/main/kotlin/lesson_7/lesson7_task4.kt
+++ src/main/kotlin/lesson_7/lesson7_task4.kt
@@ -7,11 +7,10 @@ fun main() {
     println("Введите число")
     val numberFromUser = readln().toInt()
 
-    for (i in numberFromUser downTo 1){
+    for (i in numberFromUser downTo 1) {
         Thread.sleep(MILLISECONDS_IN_SECONDS.toLong())
         println("Осталось $i секунд")
     }
-    Thread.sleep(MILLISECONDS_IN_SECONDS.toLong())
     println("Время вышло")
 
 }

75acfaae0973ea54677c073137fe528f002b6000
Rustem <mametich@rambler.ru>
2024-03-27 22:19:21 +0300

    KS-8-1-array

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -2,7 +2,7 @@ package lesson_8
 
 fun main() {
 
-    val quantityOfWatches: IntArray = intArrayOf(5, 9, 8, 15, 13, 3, 4)
+    val quantityOfWatches = arrayOf(5, 9, 8, 15, 13, 3, 4)
 
     println("Количество просмотров за неделю: ${quantityOfWatches.sum()}")
 }

b321479046b5dc5c6132a6d60cdb29ad0cf534fd
Rustem <mametich@rambler.ru>
2024-03-29 10:13:28 +0300

    KS-8-2-array

--- src/main/kotlin/lesson_8/lesson8_task2.kt
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -7,13 +7,11 @@ fun main() {
     println("Введите нужный Вам ингридиент:")
     val answerFromUser = readln()
 
-    for (i in arrayOfIngredients){
-        if (arrayOfIngredients.contains(answerFromUser)){
+    for (i in arrayOfIngredients) {
+        if (i == answerFromUser) {
             println("Ингредиент $answerFromUser в рецепте есть")
-            break
-        } else {
-            println("Такого ингредиента в рецепте нет")
             break
         }
     }
+    println("Такого ингредиента в рецепте нет")
 }

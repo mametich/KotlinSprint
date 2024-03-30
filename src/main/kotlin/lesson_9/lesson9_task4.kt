e2124d47d1776799918af4e20ed9bf3e64b828f5
Rustem <mametich@rambler.ru>
2024-03-30 18:38:21 +0300

    KS-9-4-list

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -2,10 +2,10 @@ package lesson_9
 
 fun main() {
 
-    println("Введите 5 ингредиентов через запятую:")
+    println("Введите 5 ингредиентов через запятую с пробелом:")
     val ingredientsFromUser = readln()
 
-    val listOfIngredientsFromUser = ingredientsFromUser.split(",")
+    val listOfIngredientsFromUser = ingredientsFromUser.split(", ")
     println(listOfIngredientsFromUser)
 
     val newSortListOfIngredients = listOfIngredientsFromUser.sorted()

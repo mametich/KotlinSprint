a6216289c222f29cba999213be4ca3a7c8f31f24
Rustem <mametich@rambler.ru>
2024-03-31 10:36:25 +0300

    KS-7-5-list

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -2,21 +2,19 @@ package lesson_7
 
 fun main() {
 
-    val chars = 'a'..'z'
-    val numbers = 0..9
-    val charsToUpperCase = 'A'..'Z'
+    val chars = ('a'..'z') + (0..9) + ('A'..'Z')
 
     var password = ""
 
     println("Введите количество символов к паролю от 6 символов:")
     val sizeOfPassword = readln().toInt()
+    if (sizeOfPassword < 6) {
+        println("Пароль менее 6 символов. Введите корректный пароль")
+    } else {
+        for (i in 0 until sizeOfPassword) {
+            password += chars.shuffled().random()
+        }
+        println("Ваш пароль: ${password.take(sizeOfPassword)}")
 
-    for (i in 0 until sizeOfPassword){
-        password += chars.random()
-        password += numbers.random()
-        password += charsToUpperCase.random()
     }
-    println("Ваш пароль: ${password.take(sizeOfPassword)}")
-
-
 }

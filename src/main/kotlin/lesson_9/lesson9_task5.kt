65e4afa847788b36faace977ca3c70e109abc480
Rustem <mametich@rambler.ru>
2024-04-02 22:18:39 +0300

    KS-9-5-function

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -1,27 +1,23 @@
 package lesson_9
 
-import java.util.*
-
 fun main() {
 
-    val listOfIngredientsFromUser: MutableList<String> = mutableListOf()
+    val listOfIngredientsFromUser: MutableSet<String> = mutableSetOf()
 
     println("Введите ингредиент:")
     for (i in 0 until 5) {
         val ingredientFromUser = readln()
-        if (listOfIngredientsFromUser.contains(ingredientFromUser)) {
-            println("Такой ингредиент уже существует! Введите новый:")
-        } else {
-            listOfIngredientsFromUser.add(ingredientFromUser)
-            println("Введите ингредиент:")
-        }
+        listOfIngredientsFromUser.add(ingredientFromUser)
+    }
+    val sortedListOfIngredients = listOfIngredientsFromUser.sorted()
+    val stringSortedOfIngredients = sortedListOfIngredients.joinToString()
+    val firstElementToUpperCase = stringSortedOfIngredients.replaceFirstChar {
+        it.uppercaseChar()
     }
 
-    val sortedlistOfIngredientsFromUser = listOfIngredientsFromUser.sorted()
-    val listWithUpperCaseIngredients = sortedlistOfIngredientsFromUser.map {
-        it.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
-    }
-
-    println(listWithUpperCaseIngredients)
+    println(firstElementToUpperCase)
 }
+
+
+
 

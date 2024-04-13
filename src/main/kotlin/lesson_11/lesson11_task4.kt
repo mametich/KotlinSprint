e71c45713d91777a09afbec2bfdee472f01db7a4
Rustem <mametich@rambler.ru>
2024-04-13 15:32:21 +0300

    KS-11-4-function

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -1,28 +1,25 @@
 package lesson_11
 
 fun main() {
-
-
-
 }
 
 class Category(
     val imageOfCategory: Int,
     val nameOfCategory: String,
-    val listOfCategory: MutableList<String> =
+    val listOfCategory: MutableList<Recipe> =
         mutableListOf(),
 )
 
 class Recipe(
     val imageOfRecipe: Int,
     val nameOfRecipe: String,
-    val textIngredients: String,
+    val textIngredients: List<Ingredient>,
     val quantityOfPortion: Int,
 )
 
-class Ingredients(
-    val nameOfIngredients: String,
-    val quantityOfIngredients: Int,
+class Ingredient(
+    val nameOfIngredient: String,
+    val quantityOfIngredient: Int,
     val weightOfMeasure: String,
 
 )

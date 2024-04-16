d5ac8a50aec851c35a20eeb3f57e00a694c6880b
Rustem <mametich@rambler.ru>
2024-04-16 21:48:05 +0300

    KS-10-5-function

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -2,38 +2,40 @@ package lesson_10
 
 fun main() {
 
-    println("Введите логин:")
-    val newLoginFromUser = readln()
-
-    println("Введите пароль:")
-    val newPasswordFromUser = readln()
-
     println("Введите Ваш логин:")
     val loginOfUser = readln()
 
     println("Введите Ваш пароль:")
     val passwordOfUser = readln()
 
-    println(
-        checkedLoginAndPasswordAgain(newLoginFromUser, newPasswordFromUser, loginOfUser, passwordOfUser)
-    )
+    getListOfBasket(checkedLoginAndPasswordAgain(loginOfUser, passwordOfUser).toString())
 }
 
-fun checkedLoginAndPasswordAgain(
-    login: String,
-    password: String,
-    currentLogin: String,
-    currentPassword: String
-): String {
-    if (login == currentLogin && password == currentPassword) {
-        var token = ""
-        val rangeOfToken = (0..9) + ('a'..'z')
+fun checkedLoginAndPasswordAgain(currentLogin: String, currentPassword: String) : Boolean {
+
+    val correctLogin = "user"
+    val correctPassword = "qwerty"
+    var token: String? = null
+    val rangeOfToken = (0..9) + ('a'..'z')
+
+    if (correctLogin == currentLogin && correctPassword == currentPassword) {
         for (i in 1..32) {
             token += rangeOfToken.random()
         }
-        return token
+        return true
     } else {
-        return "Вы неправильно ввели пароль"
+        return false
     }
-
 }
+
+fun getListOfBasket(token: String?) : String {
+    val listOfBasket = mutableListOf("apple", "banana", "mango")
+
+    if (token != null) {
+        println(listOfBasket)
+    } else {
+        println("Неправильный пароль")
+    }
+    return listOfBasket.toString()
+}
+

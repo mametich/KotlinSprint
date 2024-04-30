be1a7022392c88110f0f6b014b619afbfe37b343
Rustem <mametich@rambler.ru>
2024-04-30 12:15:57 +0300

    KS-16-2-encapsulation

---
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -0,0 +1,27 @@
+package lesson_16
+
+import kotlin.math.pow
+
+fun main() {
+
+    val circle = Circle(6.2)
+    println(circle.theAreaOfTheCircle())
+    println(circle.circumferenceOfTheCircle())
+
+}
+
+class Circle(
+    private var radius: Double
+) {
+
+    private val numberOfPi = 3.14
+
+    fun circumferenceOfTheCircle(): Double {
+        return 2 * numberOfPi * radius
+    }
+
+    fun theAreaOfTheCircle(): Double {
+        return (numberOfPi * radius).pow(2)
+    }
+
+}

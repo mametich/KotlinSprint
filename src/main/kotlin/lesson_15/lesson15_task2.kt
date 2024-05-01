8abfffec0a4f5cf58b7454e0e58b0d97081277c5
Rustem <mametich@rambler.ru>
2024-05-01 10:45:43 +0300

    KS-15-2-encapsulation

--- src/main/kotlin/lesson_15/lesson15_task2.kt
+++ src/main/kotlin/lesson_15/lesson15_task2.kt
@@ -12,25 +12,12 @@ fun main() {
 
 }
 
-class WeatherServer {
-
-val weatherServer = WeatherServer()
-
-val temperature = Temperature(27.0)
-val precipitationAmount = PrecipitationAmount(true)
-
-weatherServer.sendInformationToServer(temperature)
-weatherServer.sendInformationToServer(precipitationAmount)
-
-}
-
 class WeatherServer() {
 
     fun sendInformationToServer(weatherStationStats: WeatherStationStats) {
         val stringInformation = when (weatherStationStats) {
             is Temperature -> "Температура: ${weatherStationStats.informationAboutTemperature}"
             is PrecipitationAmount -> "Наличие осадков: ${weatherStationStats.quantityOfPrecipitation}"
-            is PrecipitationAmount -> "Наличие осадков: ${weatherStationStats.isHaveRain}"
             else -> "Загрузите информацию"
         }
         println(stringInformation)

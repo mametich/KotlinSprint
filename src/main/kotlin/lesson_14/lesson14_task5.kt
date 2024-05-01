ad5583b495ce458dc346dd49d51cf64fe690d563
Rustem <mametich@rambler.ru>
2024-05-01 12:23:17 +0300

    KS-14-5-interface

--- src/main/kotlin/lesson_14/lesson14_task5.kt
+++ src/main/kotlin/lesson_14/lesson14_task5.kt
@@ -5,6 +5,8 @@ fun main() {
     val chat = Chat()
     chat.addMessage("Harry", "Hello Ron! How are you?")
     chat.addThreadMessage("Ron", "Harry", "I am fine! And you?")
+    chat.addMessage("Germione", "What are you doing?")
+    chat.addThreadMessage("Ron", "Germione", "We are fighting!")
 
     chat.printChat()
 
@@ -14,25 +16,30 @@ class Chat {
 
     private val listOfMessage = mutableListOf<Message>()
 
-    fun addMessage(messageId: String, text: String) : Message {
-        val message = Message(messageId, text)
+    fun addMessage(authorId: String, text: String): Message {
+        val message = Message(authorId, text)
         listOfMessage.add(message)
         return message
     }
 
-    fun addThreadMessage(messageId: String, parentMessageId: String, text: String) : ChildMessage {
-        val childMessage = ChildMessage(messageId, parentMessageId, text)
+    fun addThreadMessage(authorId: String, parentMessageId: String, text: String): ChildMessage {
+        val childMessage = ChildMessage(authorId, parentMessageId, text)
         listOfMessage.add(childMessage)
         return childMessage
     }
 
     fun printChat() {
-        println("${listOfMessage.groupBy { it.messageId }}")
+
+        listOfMessage.groupBy { it.authorId }
+
+        listOfMessage.forEach {
+            println("${it.authorId} : ${it.text}")
+        }
     }
 }
 
-open class Message(val messageId: String, val text: String)
+open class Message(val authorId: String, val text: String)
 
-class ChildMessage(messageId: String, val parentMessageId: String, text: String,) : Message(messageId,text)
+class ChildMessage(authorId: String, val parentMessageId: String, text: String) : Message(authorId, text)
 
 

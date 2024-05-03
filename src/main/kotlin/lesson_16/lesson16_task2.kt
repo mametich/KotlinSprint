af547f8972beec8bc755ceffc6559aaeeb576598
Rustem <mametich@rambler.ru>
2024-05-03 11:35:58 +0300

    KS-16-2-encapsulation

--- src/main/kotlin/lesson_16/lesson16_task2.kt
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -6,8 +6,8 @@ private const val NUMBER_OF_PI = 3.14
 fun main() {
 
     val circle = Circle(6.2)
-    println(circle.theAreaOfTheCircle())
-    println(circle.circumferenceOfTheCircle())
+    println(circle.getAreaOfTheCircle())
+    println(circle.getCircumferenceOfTheCircle())
 
 }
 
@@ -15,11 +15,11 @@ class Circle(
     private var radius: Double
 ) {
 
-    fun circumferenceOfTheCircle(): Double {
+    fun getCircumferenceOfTheCircle(): Double {
         return 2 * NUMBER_OF_PI * radius
     }
 
-    fun theAreaOfTheCircle(): Double {
+    fun getAreaOfTheCircle(): Double {
         return (NUMBER_OF_PI * radius).pow(2)
     }
 }

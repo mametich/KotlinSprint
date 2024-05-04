44c26f6bfee88018da66fb438e6b03a2e32143d1
Rustem <mametich@rambler.ru>
2024-05-04 11:44:31 +0300

    KS-18-2-polymorphism

---
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -0,0 +1,35 @@
+package lesson_18
+
+fun main() {
+
+    val diceWithFour = DiceWithFour()
+    val diceWithSix = DiceWithSix()
+    val diceWithEight = DiceWithEight()
+
+    val listOfDice = listOf(diceWithFour, diceWithSix, diceWithEight)
+
+
+        listOfDice.forEach {
+            println(it.rollTheDice())
+        }
+
+
+}
+
+open class DiceWithFour() {
+    open fun rollTheDice() {
+        println("Выпало: ${(1..4).random()}")
+    }
+}
+
+class DiceWithSix() : DiceWithFour() {
+    override fun rollTheDice() {
+        println("Выпало: ${(1..6).random()}")
+    }
+}
+
+class DiceWithEight() : DiceWithFour() {
+    override fun rollTheDice() {
+        println("Выпало: ${(1..8).random()}")
+    }
+}

921c480a85c4a6614afa705c2fa454bd95382d46
Rustem <mametich@rambler.ru>
2024-05-06 12:57:42 +0300

    KS-18-2-polymorphism

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -2,34 +2,29 @@ package lesson_18
 
 fun main() {
 
-    val diceWithFour = DiceWithFour()
-    val diceWithSix = DiceWithSix()
-    val diceWithEight = DiceWithEight()
+    val diceWithFour = DiceWithFour(4)
+    val diceWithSix = DiceWithSix(6)
+    val diceWithEight = DiceWithEight(8)
 
     val listOfDice = listOf(diceWithFour, diceWithSix, diceWithEight)
-
-
-        listOfDice.forEach {
-            println(it.rollTheDice())
-        }
-
+    showDiceOfRoll(listOfDice)
 
 }
 
-open class DiceWithFour {
-    open fun rollTheDice() {
-        println("Выпало: ${(1..4).random()}")
+fun showDiceOfRoll(list: List<Dice>) {
+    list.forEach {
+        println(it.rollTheDice())
     }
 }
 
-class DiceWithSix : DiceWithFour() {
-    override fun rollTheDice() {
-        println("Выпало: ${(1..6).random()}")
+open class Dice(private var numberOfSides: Int) {
+    open fun rollTheDice() : String {
+        return "Кубик из $numberOfSides сторон. Выпало: ${(1..numberOfSides).random()}"
     }
 }
 
-class DiceWithEight : DiceWithFour() {
-    override fun rollTheDice() {
-        println("Выпало: ${(1..8).random()}")
-    }
-}
+class DiceWithFour(numberOfSides: Int) : Dice(numberOfSides)
+
+class DiceWithSix(numberOfSides: Int) : Dice(numberOfSides)
+
+class DiceWithEight(numberOfSides: Int) : Dice(numberOfSides)

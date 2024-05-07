13e904818be3079577bdf634b1d797a55a9fc7a4
Rustem <mametich@rambler.ru>
2024-05-07 09:49:23 +0300

    KS-18-3-polymorphism

--- src/main/kotlin/lesson_18/lesson18_task3.kt
+++ src/main/kotlin/lesson_18/lesson18_task3.kt
@@ -2,11 +2,11 @@ package lesson_18
 
 fun main() {
 
-    val fox: Animals = Fox("Лиса","Ягоды")
+    val fox: Animals = Fox("Лиса", "Ягоды")
     val dog: Animals = Dog("Собака", "Кости")
     val cat: Animals = Cat("Кошка", "Рыба")
 
-    val listOfAnimals = listOf(fox,dog,cat)
+    val listOfAnimals = listOf(fox, dog, cat)
     showNameOfFoodAnimals(listOfAnimals)
 
 }
@@ -30,6 +30,6 @@ open class Animals(var name: String, private var nameOfFood: String) {
 
 class Fox(name: String, nameOfFood: String) : Animals(name, nameOfFood)
 
-class Dog(name: String, nameOfFood: String) : Animals(name,nameOfFood)
+class Dog(name: String, nameOfFood: String) : Animals(name, nameOfFood)
 
-class Cat(name: String,nameOfFood: String) : Animals(name,nameOfFood)
+class Cat(name: String, nameOfFood: String) : Animals(name, nameOfFood)

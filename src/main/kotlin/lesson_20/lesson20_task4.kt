964a7de02cda64e1045d3461d22c48f568d7d2c9
Rustem <mametich@rambler.ru>
2024-05-09 22:35:05 +0300

    KS-20-4-lambda

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -4,9 +4,11 @@ fun main() {
 
     val listOfString = listOf("Кнопка1", "Кнопка2", "Кнопка3", "Кнопка4", "Кнопка5")
 
-    val newListOfString = listOfString
-        .map { println("Кнопка нажата ${listOfString}") }
+    val newListOfString = listOfString.map { element: String -> "Кнопка нажата ${element}" }
 
-    println(newListOfString)
+    val filteredList = newListOfString.filterIndexed { index, _ -> index % 2 != 0 }
 
+    filteredList.forEach {
+        println(it)
+    }
 }

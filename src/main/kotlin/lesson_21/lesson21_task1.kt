3553fa77c16b0af5a343e5206404a8fbf1f362c9
Rustem <mametich@rambler.ru>
2024-05-11 14:20:05 +0300

    KS-21-1-extension

---
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -0,0 +1,18 @@
+package lesson_21
+
+fun main() {
+
+    val word = "Hello"
+    println(word.vowelCount(word))
+
+}
+
+fun String.vowelCount(word : String) : Int {
+    var count = 0
+    for (letter in word) {
+        if (letter == 'a'|| letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'y' )
+        count++
+        continue
+    }
+    return count
+}

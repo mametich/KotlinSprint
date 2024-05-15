9b4a5abbb668103b24a8583b8c15352983b852ce
Rustem <mametich@rambler.ru>
2024-05-15 21:35:20 +0300

    KS-19-4-enum

--- src/main/kotlin/lesson_19/lesson19_task4.kt
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -3,39 +3,38 @@ package lesson_19
 fun main() {
 
     val tank = Tank()
-    println(tank.charge(Ammo.BLUE))
-    println(tank.shoot(Ammo.BLUE))
-    println(tank.charge(Ammo.GREEN))
-    println(tank.shoot(Ammo.GREEN))
-    println(tank.charge(Ammo.RED))
-    println(tank.shoot(Ammo.RED))
+    tank.charge(Ammo.BLUE)
+    println(tank.shoot())
+    tank.charge(Ammo.GREEN)
+    println(tank.shoot())
+    tank.charge(Ammo.RED)
+    println(tank.shoot())
 }
 
 enum class Ammo(var damage: Int, var nameOfAmmo: String) {
-    RED( 20, "Красный"),
-    BLUE( 5, "Синий"),
+    RED(20, "Красный"),
+    BLUE(5, "Синий"),
     GREEN(10, "Зеленый");
 }
 
 class Tank {
 
-    var ammoForCharge: Ammo? = null
+    private var ammoForCharge: Ammo? = null
 
-    fun shoot(ammo: Ammo): String {
-        if (ammoForCharge != null) {
-            return when (ammo) {
+    fun charge(ammo: Ammo) {
+        ammoForCharge = ammo
+    }
+
+    fun shoot(): String {
+        return if (ammoForCharge != null) {
+            when (ammoForCharge) {
                 Ammo.BLUE -> "Танк выстрелил ${Ammo.BLUE.damage} урона"
                 Ammo.GREEN -> "Танк выстрелил ${Ammo.GREEN.damage} урона"
                 Ammo.RED -> "Танк выстрелил ${Ammo.RED.damage} урона"
+                null -> "Необходимо зарядить патроны"
             }
         } else {
-            return "Необходимо зарядить патроны"
+            "Необходимо зарядить патроны"
         }
     }
-
-    fun charge(ammo: Ammo): String = "Танк заряжен ${ammo.nameOfAmmo}"
-
 }
-
-
-

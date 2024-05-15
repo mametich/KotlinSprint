b3c445aa7203c1ac00e64e4400c580b80193c538
Rustem <mametich@rambler.ru>
2024-05-15 22:24:27 +0300

    KS-21-1-lambda

--- src/main/kotlin/lesson_21/lesson21_task1.kt
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -3,16 +3,10 @@ package lesson_21
 fun main() {
 
     val word = "Hello"
-    println(word.vowelCount(word))
+    println(word.vowelCount())
 
 }
 
-fun String.vowelCount(word : String) : Int {
-    var count = 0
-    for (letter in word) {
-        if (letter == 'a'|| letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'y' )
-        count++
-        continue
-    }
-    return count
+fun String.vowelCount() : Int {
+    return count { it in "aouie" }
 }

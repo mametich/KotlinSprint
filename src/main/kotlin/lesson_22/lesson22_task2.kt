f2f38dadb289a926b1e0a3b1dc7dd89db316e119
Rustem <mametich@rambler.ru>
2024-05-18 18:28:56 +0300

    KS-22-2-data_class

---
+++ src/main/kotlin/lesson_22/lesson22_task2.kt
@@ -0,0 +1,24 @@
+package lesson_22
+
+fun main() {
+
+    val regularBook2 = RegularBook2("Гарри Поттер", "Джоан Роулинг")
+    val dataBook2 = DataBook2("Властелин колец", "Джон Рональд Руэл Толкин")
+
+    //в обычном классе не переопредлен метод toString() поэтому печатается хэш код в 16-разрядном виде
+    println(regularBook2)
+
+    // дата классе метод toString() переопределен и печатает строку
+    println(dataBook2)
+
+}
+
+class RegularBook2(
+    var name: String,
+    var author: String,
+)
+
+data class DataBook2(
+    var name: String,
+    var author: String,
+)

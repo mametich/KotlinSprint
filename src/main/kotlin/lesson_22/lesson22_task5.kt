d1bb592a8e8dbf5793e27c2971a8c05e5b00378e
Rustem <mametich@rambler.ru>
2024-05-21 00:37:17 +0300

    KS-22-5-data_class

--- src/main/kotlin/lesson_22/lesson22_task5.kt
+++ src/main/kotlin/lesson_22/lesson22_task5.kt
@@ -2,22 +2,27 @@ package lesson_22
 
 fun main() {
 
-val alphaCentauri = GalacticGuide(
-    "Альфа Центавра",
-    "РОждение новой звезды",
-    "19.05.2024",
-    "18.39",
-    1566666
-)
-    val (name, description, date, time, distance) = alphaCentauri
-    println("""
-        Информация о планете: $name
-        Событие: $description
-        дата события: $date
-        время события: $time
-        дистанция: $distance
-    """.trimIndent())
-
+    val alphaCentauri = GalacticGuide(
+        "Альфа Центавра",
+        "РОждение новой звезды",
+        "19.05.2024",
+        "18.39",
+        1566666
+    )
+    val nameOfPlace = alphaCentauri.component1()
+    val descriptionOfPlace = alphaCentauri.component2()
+    val dateOfEvent = alphaCentauri.component3()
+    val timeOfEvent = alphaCentauri.component4()
+    val distanceOfEvent = alphaCentauri.component5()
+    println(
+        """
+        Информация о планете:$nameOfPlace 
+        Событие: $descriptionOfPlace
+        Дата события: $dateOfEvent
+        Время события: $timeOfEvent
+        Дистанция: $distanceOfEvent
+    """.trimIndent()
+    )
 }
 
 data class GalacticGuide(

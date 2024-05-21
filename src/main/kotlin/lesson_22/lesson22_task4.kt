cc1aaa9b9346897ee8bbd37c43d6692e6e42b650
Rustem <mametich@rambler.ru>
2024-05-21 22:45:42 +0300

    KS-22-4-data_class

--- src/main/kotlin/lesson_22/lesson22_task4.kt
+++ src/main/kotlin/lesson_22/lesson22_task4.kt
@@ -2,33 +2,19 @@ package lesson_22
 
 fun main() {
 
-var mainScreenViewModel = MainScreenViewModel(
-    mainScreenState = MainScreenViewModel.MainScreenState(
-        "", false
-    )
-)
-    println(mainScreenViewModel.loadData(MainScreenViewModel.StateOfData.SUCCESS))
+
 
 }
 
 class MainScreenViewModel(
     private var mainScreenState: MainScreenState,
 ) {
-    fun loadData(stateOfData: StateOfData) : MainScreenState {
-        mainScreenState = when(stateOfData) {
-            StateOfData.SUCCESS -> MainScreenState("Данные загружены", false)
-            StateOfData.IS_LOADING -> MainScreenState("Загрузка данных", true)
-            StateOfData.ERROR -> MainScreenState("Ошибка", true)
-        }
-        return mainScreenState
+    fun loadData() {
+        mainScreenState = mainScreenState.copy("Данные отсутствуют")
+        mainScreenState = mainScreenState.copy("Данные загружаются", isLoading = true)
+        mainScreenState = mainScreenState.copy("Данные загружены")
     }
 
-
-    enum class StateOfData {
-        SUCCESS,
-        IS_LOADING,
-        ERROR
-    }
 
     data class MainScreenState(
         var data: String?,
